84305f949a1392123ae62a018f08c33c34d4f295
Gene Gershanok <gene@vidyo.com>
2017-01-09 15:09:02 -0500

    Adding initial version of the sample

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# gae-simple-room
-Simple Google App Engine app that generates room tokens
+# [Vidyo.io](http://vidyo.io) Simple Room sample running on Google App Engine
+
+This Google App Engine sample illustrates the use of [Vidyo.io](http://vidyo.io) which lets endpoints join meeting rooms of their own choosing using the VidyoConnector.
+
+For more information on [Vidyo.io](http://vidyo.io) see the [Developer](https://developer.vidyo.io) section.
+
+## Getting Started
+
+1. Download this sample from the git repository
+  - `git clone https://github.com/Vidyo/gae-simple-room.git`
+1. Grab your **Developer Key** and **Appliation ID** from [Vidyo.io](http://vidyo.io)
+  - See **API Key** section in the [Dashboard](https://developer.vidyo.io/dashboard).
+1. Modify the `main.py` to add your [Vidyo.io](http://vidyo.io) credentials
+  - Change the value of `VIDYO_IO_DEVELOPER_KEY` to your **Developer Key**
+  - Change the value of `VIDYO_IO_APPLICATION_ID` to your **Appliation ID**
+1. Run the App
+  - See Google App Engine [Tutorials](https://cloud.google.com/appengine/docs/python/tutorials) and a similar [Guestbook](https://cloud.google.com/appengine/docs/python/getting-started/creating-guestbook) example for more info.
+
+## Application Details
+
+The application has two components, the index.html page and the room.html page.
+
+### index.html
+
+This is the landing page which presents the user with a dialog box to enter their "Display Name" and "Room Name". Once the **Join** button is clicked, this page redirects to the room path ex:`/demoRoom` which is served by the `room.html`. 
+
+### room.html
+
+This page is served served by `main.py` when the path contains a room name ex: `/demoRoom`. The `main.py` invokes the `Room()` class and  performs the following:
+
+1. Base64 and URL encodes the roomId ex:`demoRoom` in case it has spaces and special characters.
+1. Checks the version number. When not provided as a query parameter, it will default to the `latest` version of the `VidyoConnector.html`.
+1. Creates a random `userName`. For this example only the `DisplayName` is known so we create a random username for our Vidyo.io token.
+1. Generates a Vidyo.io token from the `userName` using the `getVidyoIOToken()` function.
+1. Creates a template variable `url_vidyoio` which contains a URL link to the `VidyoConnector.html` with all the proper query parameters.
+1. The Template engine will generate an HTML file with an IFRAME that points to the `url_vidyoio` described in `room.html`.
+
+### Parameters
+
+This sample can take the same parameters that are available for `VidyoConnector.html`. See `VidyoConnector` -> `Web` section of Vidyo.io [Developer](https://developer.vidyo.io) documentation.
+
+In Addition, the version number can be specified by the `version` query parameters. ex: `/demoRoom?version=4.1.4.7`
---
+++ main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+# Copyright 2016 Vidyo Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START imports]
+import os
+import urllib2
+import base64
+import binascii
+from datetime import datetime
+import calendar, time
+import hashlib
+import hmac
+import sys
+import random
+
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+# Developer specific parameters
+VIDYO_IO_DEVELOPER_KEY    = "[YOUR DEVELOER KEY]"
+VIDYO_IO_APPLICATION_ID   = "[YOUR APPLICATION ID]"
+TOKEN_VALID_DURATION_SECS = 600
+
+EPOCH_SECONDS = 62167219200
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+
+def getVidyoIOToken(userName):
+	type    = 'provision'
+	key     = VIDYO_IO_DEVELOPER_KEY
+	jid     = userName + "@" + VIDYO_IO_APPLICATION_ID
+	expires = TOKEN_VALID_DURATION_SECS + EPOCH_SECONDS + int(time.mktime(datetime.now().timetuple()))
+	vCard   = ""
+	
+	def to_bytes(o):
+		return str(o).encode("utf-8")
+		
+	sep = b"\0" # Separator is a NULL character
+	body = to_bytes(type) + sep + to_bytes(jid) + sep + to_bytes(expires) + sep + to_bytes(vCard)
+	mac = hmac.new(bytearray(key, 'utf8'), msg=body, digestmod=hashlib.sha384).digest()
+	## Combine the body with the hex version of the mac
+	serialized = body + sep + binascii.hexlify(mac)
+	b64 = base64.b64encode(serialized)
+	token = b64.encode("utf8")
+	encoded_token = urllib2.quote(token)
+	return encoded_token;
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+	def get(self):
+		template_values = {}
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+# [END main_page]
+
+# [START Room]
+class Room(webapp2.RequestHandler):
+
+	def get(self, roomId):
+		# RoomID must not have spaces or special characters. Base64 encode will ensure that.
+		roomIdBase64AndEncoded = urllib2.quote(base64.b64encode(roomId))
+		# Pick the VidyoIO client version or select latest
+		version = self.request.get('version', 'latest')
+		# Create a random username for each user
+		username = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+		
+		encoded_token = getVidyoIOToken(username)
+		url_vidyoio = "https://static.vidyo.io/" + version + "/connector/VidyoConnector.html?host=vidyostaging.io&resourceId=" + roomIdBase64AndEncoded + "&token=" + encoded_token + "&hideConfig=1&" + self.request.query_string
+		template_values = {
+			'url_vidyoio': url_vidyoio,
+		}
+		template = JINJA_ENVIRONMENT.get_template('room.html')
+		self.response.write(template.render(template_values))
+
+# [END Room]
+
+# [START app]
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/([^/]+)?', Room),
+], debug=True)
+# [END app]
